e455f777303744ab4cfd2ec8f3bd274d82295aa2
BroMine <77886876+BroMinee@users.noreply.github.com>
2024-03-17 21:15:06 +0100

    [DATA] Add data in json files, fix price calculator and current production calculator (without upgrade)

--- src/Components/Building/BuildingList.jsx
+++ src/Components/Building/BuildingList.jsx
@@ -2,7 +2,6 @@ import React, {useEffect} from "react";
 import {useState} from "react";
 import "./BuildingList.css";
 import useSWR from 'swr';
-import {isVisible} from "@testing-library/user-event/dist/utils";
 
 
 const fetcher = (...args) => fetch(...args, {
@@ -17,7 +16,8 @@ const BuildingList = () => {
         data: buildingList,
         error,
         isValidating,
-    } = useSWR('/Building.json', fetcher);
+    } = useSWR(process.env.PUBLIC_URL + '/Building.json', fetcher);
+
 
     if (error) return <div>Failed to load</div>
     if (isValidating) return <div>Loading...</div>
@@ -33,9 +33,10 @@ const BuildingList = () => {
             return "/ImgBuilding/" + (index + 1) + ".png";
     }
 
+
     function convertToInt(str) {
         if (typeof str === "number")
-            return parseInt(str)
+            return str
         else if (typeof str === "string") {
             return parseInt(str.replace(/\s/g, ''))
         } else
@@ -47,7 +48,7 @@ const BuildingList = () => {
         if (typeof str === "number")
             return parseFloat(str)
         else if (typeof str === "string") {
-            return parseFloat(str.replace(/\s/g, '').replace(/,/g, '.'))
+            return parseFloat(str.replace(/,/g, '.'))
         } else
             console.error("Error in convertToFloat function")
         return -1
@@ -57,7 +58,7 @@ const BuildingList = () => {
         <ul className={"ul-horizontal"}>
             {
                 buildingList && buildingList.map((building, index) => (
-                    <Building buildingName={building["building_name"]} imgPath={getImgPath(index, building["price"])}
+                    <Building buildingName={building["name"]} imgPath={getImgPath(index, building["price"])}
                               baseProductionLevel0={convertToFloat(building["base_production"])}
                               priceLevel0={convertToInt(building["price"])}/>
                 ))
@@ -67,20 +68,40 @@ const BuildingList = () => {
 }
 
 
-const Building = ({buildingName, imgPath, baseProductionLevel0, priceLevel0}) => {
+const Building = ({buildingName, imgPath, baseProductionLevel0, priceLevel0, upgrades}) => {
     const [baseProduction, setBaseProduction] = useState(baseProductionLevel0)
+    const [currentProduction, setCurrentProduction] = useState(baseProductionLevel0)
     const [price, setPrice] = useState(priceLevel0)
-    const [level, setLevel] = useState(1)
+    const [level, setLevel] = useState(0)
+
+
+    useEffect(() => {
+        setPrice(ComputePrice(priceLevel0, level))
+        setBaseProduction(scaleBaseProduction(baseProductionLevel0, level, upgrades))
+        setCurrentProduction(scaleCurrentProduction(level, upgrades))
+        console.log(`You bought a ${buildingName} for ${price} coins!`)
+
+    }, [level])
 
     const buy = () => {
         if (level === 99) {
             console.log("You have reached the maximum level for this building!")
             return
         }
-        setPrice(priceLevel0 * Math.pow(1.10, level))
-        setBaseProduction(baseProduction * level)
         setLevel(level + 1)
-        console.log(`You bought a ${buildingName} for ${price} coins!`)
+    }
+
+    function ComputePrice(priceLevel0, level) {
+        return Math.round(priceLevel0 * Math.pow(1.100000023841858, level))
+    }
+
+    function scaleBaseProduction(baseProduction, upgrades) {
+        console.log("TODO upgrade") // TODO
+        return (baseProduction * 1);
+    }
+
+    function scaleCurrentProduction(level) {
+        return baseProduction* level;
     }
 
     function printPricePretty(price) {
@@ -94,6 +115,7 @@ const Building = ({buildingName, imgPath, baseProductionLevel0, priceLevel0}) =>
                 <div className="cornerLink">{buildingName}</div>
             </div>
             <li>Base Production: {printPricePretty(baseProduction.toFixed(2))}</li>
+            <li>Current Production : {printPricePretty(currentProduction.toFixed(2))}</li>
             <li>Level: {level}</li>
             <div>Price : {printPricePretty(price)}$</div>
             <button onClick={buy}>Buy</button>
