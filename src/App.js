f53a50eff18913c0951d11c0c19f238eec9e2fe3
BroMine <77886876+BroMinee@users.noreply.github.com>
2024-04-11 15:35:04 +0200

    [Data] Adding last building

--- src/App.js
+++ src/App.js
@@ -285,7 +285,7 @@ export function isCacheDateValid() {
     }
     try {
         const jsonCacheInfo = JSON.parse(cacheInfo);
-        if (jsonCacheInfo["timestamp"] < new Date("08 April 2024 17:22 UTC+2")) {
+        if (jsonCacheInfo["timestamp"] < new Date("11 April 2024 15:40 UTC+2")) {
             return false
         }
     } catch (e) {
