1001c13100c4b51aecb40c76c8b04318bdb2e9e6
BroMine <77886876+BroMinee@users.noreply.github.com>
2024-04-22 20:35:50 +0200

    [UI] Changement du design des métiers

--- src/Components/Metier/MetierList.js
+++ src/Components/Metier/MetierList.js
@@ -1,5 +1,6 @@
 import React from "react";
-import {useState} from "react";
+
+import {SlArrowUp, SlArrowDown} from "react-icons/sl";
 
 import "./MetierList.css"
 
@@ -118,9 +119,9 @@ const metierPalier = [
 ]
 
 function getXpForLevel(level, currentXp) {
-    if(level === 100)
+    if (level === 100)
         return 1
-    if(currentXp === 0)
+    if (currentXp === 0)
         return 0
     const sum = metierPalier.slice(0, level).reduce((a, b) => a + b, 0)
     return (currentXp - sum) / metierPalier[level]
@@ -144,22 +145,25 @@ const Metier = ({metierName, imgPath, playerInfo, setPlayerInfo, level}) => {
             return true;
         }
     }
-
-
     let rgb = [0, 150, 0]
 
+    let bgc = [0, 0, 0]
     switch (metierName) {
         case "mineur":
             rgb = [255, 47, 47]
+            bgc = [255, 47, 47]
             break;
         case "farmer":
             rgb = [199, 169, 33]
+            bgc = [255, 209, 1]
             break;
         case "hunter":
             rgb = [47, 103, 255]
+            bgc = [47, 103, 255]
             break;
         case "alchimiste":
             rgb = [255, 100, 201]
+            bgc = [255, 100, 201]
     }
 
     const coefXp = getXpForLevel(level, playerInfo["metier"].find((metier) => metier["name"] === metierName)["xp"] || 0)
@@ -175,7 +179,7 @@ const Metier = ({metierName, imgPath, playerInfo, setPlayerInfo, level}) => {
                               d="M723 314L543 625.77 183 625.77 3 314 183 2.23 543 2.23 723 314z"></path>
                         <path className="fill" d="M723 314L543 625.77 183 625.77 3 314 183 2.23 543 2.23 723 314z"
                               style={{
-                                  strokeDashoffset: 2160 *  (1 - (coefXp)),
+                                  strokeDashoffset: 2160 * (1 - (coefXp)),
                                   stroke: `rgb(${rgb[0]},${rgb[1]},${rgb[2]})`,
                               }}/>
                     </svg>
@@ -183,9 +187,35 @@ const Metier = ({metierName, imgPath, playerInfo, setPlayerInfo, level}) => {
 
                 <div className="cornerLink">{metierName.charAt(0).toUpperCase() + metierName.slice(1)}</div>
             </div>
-            <li>Level: {level}</li>
-            <input type="number" min="1" step="1" max="100" value={level} onKeyUp={enforceMinMax}
-                   onChange={enforceMinMax}/>
+            <div className={"Lvl-txt ul-horizontal"}
+                 style={{backgroundColor: `rgb(${bgc[0]},${bgc[1]},${bgc[2]})`}}>
+                <div>
+                    <input className={"Lvl-inside-txt"} type="number" min="1" step="1" max="100" value={level} onKeyUp={enforceMinMax}
+                           onChange={enforceMinMax}/>
+                </div>
+                <div className={"ArrowUpDown"}>
+
+                    <div onClick={() => {
+                        if(playerInfo["metier"].find((metier) => metier["name"] === metierName)["level"] === 100)
+                            return
+                        playerInfo["metier"].find((metier) => metier["name"] === metierName)["level"] += 1
+                        playerInfo["metier"].find((metier) => metier["name"] === metierName)["xp"] = 0
+                        setPlayerInfo({...playerInfo})
+                    }}>
+                        <SlArrowUp/>
+                    </div>
+                    <div onClick={() =>
+                    {
+                        if(playerInfo["metier"].find((metier) => metier["name"] === metierName)["level"] === 1)
+                            return
+                        playerInfo["metier"].find((metier) => metier["name"] === metierName)["level"] -= 1
+                        playerInfo["metier"].find((metier) => metier["name"] === metierName)["xp"] = 0
+                        setPlayerInfo({...playerInfo})
+                    }}>
+                        <SlArrowDown/>
+                    </div>
+                </div>
+            </div>
         </ul>
     )
         ;
