35c2b142292585ab815f36c12ab5c3555aee49a2
None <None>
2024-05-19 23:02:33 +0200

    [UI] fix footer to always be at the bottom of the page even on phone

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -8,19 +8,19 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <>
+    <div style={{ position: "relative", minHeight: "100vh"}}>
       <header className="h-16 w-full bg-primary backdrop-blur text-primary-foreground sticky top-0 z-10 border-b">
         <Navbar />
       </header>
-      <main className="container py-4">
+      <main className="container py-4 pb-16">
         {children}
       </main>
-      <footer className="p-4 bg-accent text-center">
+      <footer className="p-4 bg-accent text-center w-full absolute bottom-0 h-14">
         L'application est{" "}
         <span className="font-bold">ni affiliée ni approuvée</span>{" "}
         par <GradientText><a href="https://paladium-pvp.fr/" target="_blank">Paladium Games</a></GradientText>.
       </footer>
-    </>
+    </div>
   );
 }
 
