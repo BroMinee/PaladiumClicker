d33ede541c24de6cd55718416107af1470796982
BroMine <77886876+BroMinee@users.noreply.github.com>
2024-06-18 18:54:02 +0200

    [data] leaderboard / daily popup

--- src/components/dailyPopup.tsx
+++ src/components/dailyPopup.tsx
@@ -13,14 +13,26 @@ const DailyPopup = ({defaultOpen = false}) => {
             <CardHeader>
               <CardTitle className="text-secondary-foreground flex flex-col gap-2">
                 <div> Je suis à la recherche de :</div>
-                <div>- Endium ingot</div>
-                <div>- Endium nugget</div>
-                <div>- Endium pickaxe</div>
-                <div>- Endium axe</div>
-                <div>- Endium battery</div>
-                <div>- Cloche magique en endium</div>
-                <div>- Endium stone heart</div>
-                <div>- Endium dynamite (même si je crois qu'il y en a pas en circulation)</div>
+                <div className="flex flex-row gap-2 items-center">
+                  <img src={import.meta.env.BASE_URL + "/AH_img/endium_battery.png"} className="h-6 w-6 pixelated"
+                       alt="Endium baterry"/>
+                  <div>Endium battery</div>
+                </div>
+                <div className="flex flex-row gap-2 items-center">
+                  <img src={import.meta.env.BASE_URL + "/AH_img/endium_bell.png"} className="h-6 w-6 pixelated"
+                       alt="Endium bell"/>
+                  <div>Cloche magique en endium</div>
+                </div>
+                <div className="flex flex-row gap-2 items-center">
+                  <img src={import.meta.env.BASE_URL + "/AH_img/endium_stone_heart.png"} className="h-6 w-6 pixelated"
+                       alt="Endium Stone heart"/>
+                  <div>Endium stone heart</div>
+                </div>
+                <div className="flex flex-row gap-2 items-center">
+                  <img src={import.meta.env.BASE_URL + "/AH_img/dynamite_endium.png"} className="h-6 w-6 pixelated"
+                       alt="Endium Dynamite"/>
+                  <div>Endium dynamite (même si je crois qu'il y en a pas en circulation)</div>
+                </div>
                 <div>Si vous en avez mp moi discord : bromine__</div>
               </CardTitle>
             </CardHeader>
