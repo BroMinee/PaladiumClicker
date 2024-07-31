0a26c51125df4dfb606f2b23d696e27f98583727
Rayan-Walnut <gith945@gmail.com>
2024-07-31 23:37:33 +0200

    Feat : ajout d'un effet de hover Paladium Games

--- src/components/shared/Layout.tsx
+++ src/components/shared/Layout.tsx
@@ -8,18 +8,26 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div style={{ position: "relative", minHeight: "100vh" }}>
-      <header className="h-16 w-full bg-primary backdrop-blur text-primary-foreground sticky top-0 z-10 border-b">
-        <Navbar/>
+    <div className="relative min-h-screen flex flex-col">
+      <header className="h-16 w-full bg-primary text-primary-foreground sticky top-0 z-10 border-b border-primary-200 backdrop-blur-md">
+        <Navbar />
       </header>
-      <main className="container py-4 pb-16">
+      <main className="flex-1 container py-4 pb-16 mx-auto">
         {children}
       </main>
-      <footer className="p-4 bg-accent text-center w-full absolute bottom-0 h-14">
+      <footer className="p-4 bg-accent text-center w-full border">
         L'application est{" "}
         <span className="font-bold">ni affiliée ni approuvée</span>{" "}
-        par <GradientText><a href="https://paladium-pvp.fr/" target="_blank">Paladium
-        Games</a></GradientText>.
+        par <GradientText>
+          <a
+            href="https://paladium-pvp.fr/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="text-orange-500 hover:text-orange-700 transition-colors duration-300"
+          >
+            Paladium Games
+          </a>
+        </GradientText>.
       </footer>
     </div>
   );
