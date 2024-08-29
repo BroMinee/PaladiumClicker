625ce65122ae3a6e2feacbaedbe66e917baaf0ff
BroMine <77886876+BroMinee@users.noreply.github.com>
2024-08-29 14:19:17 +0200

    [refactor] adding dynamic MetaHeader to Ah Tracker

--- src/app/ah/page.tsx
+++ src/app/ah/page.tsx
@@ -16,86 +16,115 @@ import itemListJson from "@/assets/items_list.json";
 import LoadingSpinner from "@/components/ui/loading-spinner.tsx";
 import MarketSelector from "@/components/AhTracker/MarketSelector.tsx";
 import QuantitySelectorDisplay from "@/components/AhTracker/QuantitySelectorDisplay.tsx";
+import { GetAllFileNameInFolder, levensteinDistance } from "@/lib/misc.ts";
 
-export default function AhTrackerPage({ searchParams }: { searchParams: { item: string | undefined } }) {
-  return (
-    <div className="flex flex-col gap-4">
-      <Card>
-        <CardHeader className="flex flex-row items-center justify-between">
-          <CardTitle>
-            Bienvenue sur{" "}
-            <GradientText className="font-extrabold">l'AH Tracker</GradientText>
-          </CardTitle>
-          <CardDescription>
-            Made with <FaHeart className="text-primary inline-block"/> by <GradientText>BroMine__</GradientText>
-          </CardDescription>
-        </CardHeader>
-      </Card>
-      <Card className="bg-red-700">
-        <CardHeader>
-          <CardTitle className="text-primary-foreground">
-            Le prix de vente en $ journalier est inexact, il est calculé en divisant la somme des prix de vente
-            en $ par
-            le nombre de ventes journalières, or le nombre de ventes journalières contient aussi les ventes en pbs.
-            Il y a
-            donc une surévaluation du vrai prix.
-            Cela sera corrigé après une mise à jour de l'API de Paladium.
-          </CardTitle>
-        </CardHeader>
-      </Card>
-      <Card className="md:col-start-1 md:col-span-2 md:row-start-1 md:row-span-3 row-span-3">
-        <CardContent className="gap-2 flex flex-col pt-4">
-          <MarketSelector/>
-        </CardContent>
-        {searchParams.item ?
-          <Suspense fallback={<QuantitySelectorDisplayFallBack selectedItem={searchParams.item}/>}>
-            <QuantitySelectorDisplay selectedItem={searchParams.item}/>
-          </Suspense>
-          :
-          null}
-      </Card>
-      <div className="w-full">
-        {searchParams.item ?
-          <Suspense fallback={<GraphItemFallback selectedItem={searchParams.item}/>}>
-            <GraphItem selectedItem={searchParams.item}/>
-          </Suspense>
-          :
-          <Card className="col-start-1 col-span-4 w-full">
-            <CardHeader>
-              <CardTitle>
-                Veuillez sélectionner un item
-              </CardTitle>
-            </CardHeader>
-          </Card>}
-      </div>
-    </div>)
-}
+export function generateMetadata(
+  { searchParams }: { searchParams: { item: string | undefined } },
+) {
+  let closestItemName = searchParams.item === undefined ? "" : GetAllFileNameInFolder().reduce((acc, curr) => {
+      if (levensteinDistance(curr, searchParams.item as string) < levensteinDistance(acc, searchParams.item as string)) {
+        return curr;
+      } else {
+        return acc;
+      }
+    });
+
+    return {
+      title: `AH Tracker ${searchParams.item ? " - " + itemListJson.find((item) => item.value === searchParams.item)?.label : ""}`,
+      description: "Suivez les historiques de vente de vos items préférés sur Paladium",
+      openGraph: {
+        title: `AH Tracker ${searchParams.item ? " - " + itemListJson.find((item) => item.value === searchParams.item)?.label : ""}`,
+        description: "Suivez les historiques de vente de vos items préférés sur Paladium",
+        images: [
+          {
+            url: `https://brominee.github.io/PaladiumClicker/AH_img/${closestItemName}.png`,
+            width: 800,
+            height: 600,
+          }
+        ]
+      },
+    }
+  }
+
+  export default function AhTrackerPage({ searchParams }: { searchParams: { item: string | undefined } }) {
+    return (
+      <div className="flex flex-col gap-4">
+        <Card>
+          <CardHeader className="flex flex-row items-center justify-between">
+            <CardTitle>
+              Bienvenue sur{" "}
+              <GradientText className="font-extrabold">l'AH Tracker</GradientText>
+            </CardTitle>
+            <CardDescription>
+              Made with <FaHeart className="text-primary inline-block"/> by <GradientText>BroMine__</GradientText>
+            </CardDescription>
+          </CardHeader>
+        </Card>
+        <Card className="bg-red-700">
+          <CardHeader>
+            <CardTitle className="text-primary-foreground">
+              Le prix de vente en $ journalier est inexact, il est calculé en divisant la somme des prix de vente
+              en $ par
+              le nombre de ventes journalières, or le nombre de ventes journalières contient aussi les ventes en pbs.
+              Il y a
+              donc une surévaluation du vrai prix.
+              Cela sera corrigé après une mise à jour de l'API de Paladium.
+            </CardTitle>
+          </CardHeader>
+        </Card>
+        <Card className="md:col-start-1 md:col-span-2 md:row-start-1 md:row-span-3 row-span-3">
+          <CardContent className="gap-2 flex flex-col pt-4">
+            <MarketSelector/>
+          </CardContent>
+          {searchParams.item ?
+            <Suspense fallback={<QuantitySelectorDisplayFallBack selectedItem={searchParams.item}/>}>
+              <QuantitySelectorDisplay selectedItem={searchParams.item}/>
+            </Suspense>
+            :
+            null}
+        </Card>
+        <div className="w-full">
+          {searchParams.item ?
+            <Suspense fallback={<GraphItemFallback selectedItem={searchParams.item}/>}>
+              <GraphItem selectedItem={searchParams.item}/>
+            </Suspense>
+            :
+            <Card className="col-start-1 col-span-4 w-full">
+              <CardHeader>
+                <CardTitle>
+                  Veuillez sélectionner un item
+                </CardTitle>
+              </CardHeader>
+            </Card>}
+        </div>
+      </div>)
+  }
 
 
-function QuantitySelectorDisplayFallBack({ selectedItem }: { selectedItem: string }) {
-  return (<Card>
-    <CardHeader>
-      <CardTitle className="flex flex-row gap-2">
-        <LoadingSpinner size={4}/>
-        Chargement des informations de :{" "}
-        <GradientText
-          className="font-extrabold">{itemListJson.find((item) => item.value === selectedItem)?.label}
-        </GradientText>
-      </CardTitle>
-    </CardHeader>
-  </Card>)
-}
+  function QuantitySelectorDisplayFallBack({ selectedItem }: { selectedItem: string }) {
+    return (<Card>
+      <CardHeader>
+        <CardTitle className="flex flex-row gap-2">
+          <LoadingSpinner size={4}/>
+          Chargement des informations de :{" "}
+          <GradientText
+            className="font-extrabold">{itemListJson.find((item) => item.value === selectedItem)?.label}
+          </GradientText>
+        </CardTitle>
+      </CardHeader>
+    </Card>)
+  }
 
-function GraphItemFallback({ selectedItem }: { selectedItem: string }) {
-  return (<Card>
-    <CardHeader>
-      <CardTitle className="flex flex-row gap-2">
-        <LoadingSpinner size={4}/>
-        Chargement du graphique de prix de :{" "}
-        <GradientText
-          className="font-extrabold">{itemListJson.find((item) => item.value === selectedItem)?.label}
-        </GradientText>
-      </CardTitle>
-    </CardHeader>
-  </Card>)
-}
+  function GraphItemFallback({ selectedItem }: { selectedItem: string }) {
+    return (<Card>
+      <CardHeader>
+        <CardTitle className="flex flex-row gap-2">
+          <LoadingSpinner size={4}/>
+          Chargement du graphique de prix de :{" "}
+          <GradientText
+            className="font-extrabold">{itemListJson.find((item) => item.value === selectedItem)?.label}
+          </GradientText>
+        </CardTitle>
+      </CardHeader>
+    </Card>)
+  }
