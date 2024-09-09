17130be2abd75b7c6ac4cc91b89fb430c6c7bb41
BroMine <77886876+BroMinee@users.noreply.github.com>
2024-09-09 19:05:57 +0200

    [refacto] fix compilation cause by &apos;

--- src/app/status/page.tsx
+++ src/app/status/page.tsx
@@ -20,7 +20,7 @@ export default function Home() {
           </CardDescription>
         </CardHeader>
         <CardContent className="flex flex-col items-center justify-between gap-2 h-[calc(100vh-30vh)]">
-          Cette page n'est pas encore finie, mais vous pouvez déjà voir le graphique des joueurs connectés sur Paladium.
+          Cette page n&apos;est pas encore finie, mais vous pouvez déjà voir le graphique des joueurs connectés sur Paladium.
           <Suspense fallback={<GraphStatusFallback/>}>
             <GraphStatus/>
           </Suspense>
