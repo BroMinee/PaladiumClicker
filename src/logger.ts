13fd3812dcdddb8e36b8b186efcd6e1b503b8913
BroMine <77886876+BroMinee@users.noreply.github.com>
2024-10-18 00:20:45 +0200

    [log] adding logger to get all the requested URL

---
+++ src/logger.ts
@@ -0,0 +1,13 @@
+import winston from 'winston';
+export const logger = winston.createLogger({
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ level, message, timestamp }) => {
+      return `[${timestamp}] [${level}] - ${message}`;
+    })
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'combined.log' })
+  ],
+});
