cb55760ad251e4c42679c3f430e56964d02db24d
BroMine <77886876+BroMinee@users.noreply.github.com>
2025-02-26 20:53:11 +0100

    [robot] dynamic robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,14 @@
 import type { MetadataRoute } from 'next'
 
 export default function robots(): MetadataRoute.Robots {
+  const isProduction = process.env.NEXT_PUBLIC_PALACLICKER_API_URL === "https://palatracker.bromine.fr";
+
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
-      disallow: ["/v1/", "/error"],
+      allow: isProduction ? '/' : '',
+      disallow: isProduction ? ["/v1/", "/error"] : ['/'],
     },
-    sitemap: 'https://palatracker.bromine.fr/sitemap.xml',
+    sitemap: isProduction ? 'https://palatracker.bromine.fr/sitemap.xml' : '',
   }
 }
