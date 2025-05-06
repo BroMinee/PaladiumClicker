512438872ea061c2e2c4dd48219007ce78be210c
BroMine <77886876+BroMinee@users.noreply.github.com>
2025-05-06 19:37:43 +0200

    feat: craft optimizer v2

--- src/components/Craft/CraftOptimizerDisplay.tsx
+++ src/components/Craft/CraftOptimizerDisplay.tsx
@@ -1,23 +1,29 @@
 'use client';
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import React, { useEffect, useRef, useState } from "react";
+import React, { MutableRefObject, useEffect, useRef, useState } from "react";
 import { useCraftStore } from "@/stores/use-craft-store";
 import Image from "next/image";
-import { CraftPrice } from "@/types";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { CraftPrice, CraftSectionEnum } from "@/types";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
-import { getDDHHMMSS, parseMessageCraftPrice } from "@/lib/misc.ts";
+import { formatPrice, generateCraftUrl, getDDHHMMSS, parseMessageCraftPrice } from "@/lib/misc.ts";
 import gsap from "gsap";
 import { API_PALATRACKER_WS } from "@/lib/constants.ts";
 
 type SortMode = "profit" | "margin" | "speed" | "score";
+type CraftPriceWithComputed = CraftPrice & {
+  profit: number,
+  margin: number,
+  speed: number,
+  score: number
+}
 
 export function CraftOptimizerDisplay() {
   const { craftingList, setCraftingList } = useCraftStore();
-  const [topCraft, setTopCraft] = useState<CraftPrice[]>([]);
+  const [topCraft, setTopCraft] = useState<Array<CraftPriceWithComputed>>([]);
   const [sortMode, setSortMode] = useState<SortMode>("score");
   const [lastUpdate, setLastUpdate] = useState<Date>(new Date("1970-01-01T00:00:00Z"));
+  const [isConnected, setIsConnected] = useState<boolean>(false);
   const cardRefs = useRef<HTMLElement[]>([]);
 
   useEffect(() => {
@@ -27,30 +33,42 @@ export function CraftOptimizerDisplay() {
       if (socket.readyState === WebSocket.OPEN) {
         socket.send(JSON.stringify({ type: "ping" }));
       }
+      if (socket.readyState === WebSocket.CLOSED) {
+        setIsConnected(false);
+        clearInterval(interval);
+      }
     }, 30000);
 
-    socket.addEventListener('open', () => console.log('WebSocket ouvert'));
-    socket.addEventListener('message', (event) => {
+    socket.addEventListener("open", () => {
+      setIsConnected(true);
+      console.log("WebSocket ouvert");
+    });
+    socket.addEventListener("message", (event) => {
       const message = parseMessageCraftPrice(event.data);
       if (message.type !== "update")
         return;
       setCraftingList(message.data);
     });
-    socket.addEventListener('close', (e) => console.log(`WebSocket fermé à ${new Date().toLocaleTimeString()}`, e));
-    socket.addEventListener('error', (error) => console.error('WebSocket error:', error));
+    socket.addEventListener("close", (e) => {
+      setIsConnected(false);
+      console.log(`WebSocket fermé à ${new Date().toLocaleTimeString()}`, e)
+    });
+    socket.addEventListener("error", (error) => console.error("WebSocket error:", error));
 
     return () => {
       clearInterval(interval);
       socket.close();
     };
-  }, []);
+  }, [setCraftingList]);
 
   useEffect(() => {
-    const craftingListFiltered = craftingList.filter(c => c.priceToCraft > 0 && c.totalSold > 0).filter(c => !(c.priceToCraft > c.currentPrice && c.priceToCraft > c.averagePrice));
+    const craftingListFiltered = craftingList.filter(c => c.priceToCraft > 0 && c.totalSold > 0);//.filter(c => !(c.priceToCraft > c.currentPrice && c.priceToCraft > c.averagePrice));
+
+    // const craftingListFiltered = craftingList.filter(c => c.priceToCraft > 0 && c.totalSold > 0).filter(c => !(c.priceToCraft > c.currentPrice && c.priceToCraft > c.averagePrice));
     const minTotalSold = Math.min(...craftingListFiltered.filter(c => c.totalSold).map(c => c.totalSold));
     const maxTotalSold = Math.max(...craftingListFiltered.filter(c => c.totalSold).map(c => c.totalSold));
-    const sortedList = [...craftingListFiltered]
-      .sort((a, b) => getSortValue(b, sortMode, minTotalSold, maxTotalSold) - getSortValue(a, sortMode, minTotalSold, maxTotalSold));
+    const sortedList = craftingListFiltered.map(e => computeSortValue(e, minTotalSold, maxTotalSold))
+      .sort((a, b) => getSortValue(b, sortMode) - getSortValue(a, sortMode));
     setTopCraft(sortedList.slice(0, 100));
     if (craftingList.length === 0)
       return;
@@ -58,7 +76,6 @@ export function CraftOptimizerDisplay() {
   }, [craftingList, sortMode]);
 
   useEffect(() => {
-    // Animation des cartes lors de la mise à jour
     cardRefs.current.forEach((card, index) => {
       gsap.fromTo(card,
         { opacity: 0, x: -50 },
@@ -72,39 +89,47 @@ export function CraftOptimizerDisplay() {
       <Card className="rounded-b-xl rounded-t-none">
         <CardHeader className="flex flex-col md:flex-row md:justify-between md:items-center gap-4">
           <CardTitle>Voici la liste des items les plus rentables</CardTitle>
-          <div className="text-gray-400 text-sm">
-            Dernière mise à jour : {getDDHHMMSS(lastUpdate)}
+          <div className="flex items-center gap-2 text-gray-400 text-sm">
+            <span
+              className={`w-3 h-3 min-w-3 min-h-3 rounded-full ${isConnected ? "bg-green-500" : "bg-red-500"}`}
+              title={isConnected ? "Actualisation automatique" : "N'actualise plus automatiquement"}
+            />
+            <span>Dernière mise à jour : {getDDHHMMSS(lastUpdate)}</span>
           </div>
           <Select defaultValue="score" onValueChange={(val) => setSortMode(val as SortMode)}>
             <SelectTrigger className="w-[220px]">
               <SelectValue placeholder="Méthode de tri"/>
             </SelectTrigger>
             <SelectContent>
-              <SelectItem value="score">Score combiné</SelectItem>
-              <SelectItem value="profit">Profit brut</SelectItem>
-              <SelectItem value="margin">Marge %</SelectItem>
-              <SelectItem value="speed">Prob. vente rapide</SelectItem>
+              <SelectItem value="score" title="ratio = 0.3 × (profit / priceToCraft) + 0.7 × speed">Score
+                pondérée</SelectItem>
+              <SelectItem value="profit" title="ratio = currentPrice - priceToCraft">Profit brut</SelectItem>
+              <SelectItem value="margin" title="ratio = (currentPrice - priceToCraft) / priceToCraft">Marge
+                %</SelectItem>
+              <SelectItem value="speed" title="ratio = 1 - [(totalSoldNorm) × (0.6 × probaPrice + 0.4 × probaAverage)]">Prob.
+                vente rapide</SelectItem>
             </SelectContent>
           </Select>
         </CardHeader>
         <CardContent className="flex flex-row gap-2 pt-4 justify-center items-center w-full">
-          <ScrollArea className="flex flex-grow justify-start gap-2 h-fit">
-            <div className="grid grid-cols-4 justify-start gap-2 items-stretch">
-              {topCraft.map((item, i) => (
-                <div key={`craft-${i}`} ref={el => cardRefs.current[i] = (el ?? undefined as any)}>
-                  <CraftPriceCard data={item} index={i}/>
-                </div>
-              ))}
-            </div>
-            <ScrollBar orientation="horizontal"/>
-          </ScrollArea>
+          <div
+            className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 items-stretch justify-between">
+            {topCraft.map((item, i) => (
+              <CraftPriceCard key={`craft-${i}`} data={item} index={i} cardRefs={cardRefs} sortMode={sortMode}/>
+            ))}
+          </div>
         </CardContent>
       </Card>
     </div>
   );
 }
 
-function CraftPriceCard({ data, index }: { data: CraftPrice, index: number }) {
+function CraftPriceCard({ data, index, cardRefs, sortMode }: {
+  data: CraftPriceWithComputed,
+  index: number,
+  cardRefs: MutableRefObject<HTMLElement[]>,
+  sortMode: SortMode
+}) {
   const { priceToCraft, currentPrice, averagePrice, totalSold } = data;
 
   const getBadges = () => {
@@ -135,8 +160,11 @@ function CraftPriceCard({ data, index }: { data: CraftPrice, index: number }) {
   };
 
   return (
-    <div
-      className="bg-gray-900 rounded-2xl shadow-lg p-4 w-80 flex flex-col items-center text-white hover:scale-105 transition flex-1">
+    <a
+      className="text-card-foreground dark:bg-gray-800 duration-300 dark:hover:bg-gray-700 bg-gray-200 hover:bg-gray-300 rounded-2xl shadow-lg p-4 w-80 flex flex-col items-center text-white  transition flex-1"
+      ref={el => cardRefs.current[index] = (el ?? undefined as any)}
+      href={generateCraftUrl(data.item.item_name, 1, CraftSectionEnum.recipe)}
+    >
       <div className="relative">
         <span className="absolute top-2 left-2 bg-purple-600 text-xs px-2 py-1 rounded-full">
           #{index + 1}
@@ -151,8 +179,8 @@ function CraftPriceCard({ data, index }: { data: CraftPrice, index: number }) {
         height={64}
         unoptimized={true}
       />
-      <h2 className="text-lg font-semibold text-center">{data.item.us_trad}</h2>
-      <h3 className="text-sm text-gray-400 text-center">{data.item.fr_trad}</h3>
+      <h2 className="text-lg font-semibold text-card-foreground text-center">{data.item.us_trad}</h2>
+      <h3 className="text-sm text-card-foreground/60 text-center">{data.item.fr_trad}</h3>
 
       <div className="flex flex-wrap justify-center gap-1 mt-2">
         {getBadges().map((b, i) => (
@@ -162,25 +190,31 @@ function CraftPriceCard({ data, index }: { data: CraftPrice, index: number }) {
         ))}
       </div>
 
-      <div className="mt-2 text-sm text-gray-300 space-y-1 flex-grow">
-        <p>Prix de craft : <span className="text-green-400">{formatNumber(priceToCraft)}</span></p>
-        <p>Prix actuel : <span className="text-blue-400">{formatNumber(currentPrice)}</span></p>
-        <p>Prix moyen : <span className="text-yellow-400">{formatNumber(averagePrice)}</span></p>
+      <div className="flex flex-col mt-2 text-sm text-card-foreground space-y-1 items-center justify-center">
+        <p>Prix de craft : <span className="text-green-400">{formatPrice(Math.max(0, priceToCraft))} $</span></p>
+        <p>Prix actuel : <span className="text-blue-400">{formatPrice(Math.max(0, currentPrice))} $</span></p>
+        <p>Prix moyen : <span className="text-yellow-400">{formatPrice(Math.max(0, averagePrice))} $</span></p>
+        {sortMode === "profit" &&
+          <p>Profit : <span className="text-green-400">{formatPrice(getSortValue(data, sortMode))} $</span></p>}
+        {sortMode === "margin" &&
+          <p>Marge : <span className="text-green-400">{formatPrice(getSortValue(data, sortMode))} $</span></p>}
       </div>
-    </div>
+    </a>
   );
 }
 
-function formatNumber(value: number): string {
-  if (value <= 0) return "N/A";
-  if (value >= 1_000_000) return `${(value / 1_000_000).toFixed(2)}M`;
-  if (value >= 1_000) return `${(value / 1_000).toFixed(1)}K`;
-  return value.toString();
-}
 
-function getSortValue(item: CraftPrice, mode: SortMode, minTotalSold: number, maxTotalSold: number): number {
+function computeSortValue(item: CraftPrice, minTotalSold: number, maxTotalSold: number): CraftPriceWithComputed {
   const { priceToCraft, currentPrice, averagePrice, totalSold } = item;
-  if (priceToCraft <= 0 || currentPrice <= 0) return -Infinity;
+  if (priceToCraft <= 0 || currentPrice <= 0) {
+    return {
+      ...item,
+      profit: -Infinity,
+      margin: -Infinity,
+      speed: -Infinity,
+      score: -Infinity
+    }
+  }
 
   const profit = currentPrice - priceToCraft;
   const margin = profit / priceToCraft;
@@ -195,15 +229,24 @@ function getSortValue(item: CraftPrice, mode: SortMode, minTotalSold: number, ma
   }
 
   const score = 0.3 * margin + 0.7 * speed;
+  return {
+    ...item,
+    profit,
+    margin,
+    speed,
+    score
+  };
+}
 
+function getSortValue(item: CraftPriceWithComputed, mode: SortMode): number {
   switch (mode) {
     case "profit":
-      return profit;
+      return item.profit;
     case "margin":
-      return margin;
+      return item.margin;
     case "speed":
-      return speed;
+      return item.speed;
     case "score":
-      return score;
+      return item.score;
   }
 }
