ff9cb25acba5c10140fa8b4350567585f72a9d71
BroMine <77886876+BroMinee@users.noreply.github.com>
2025-09-20 19:53:26 +0200

    [Animation] fix hydratation error when redirecting before the end the animation

--- src/components/shared/PageTransition.tsx
+++ src/components/shared/PageTransition.tsx
@@ -1,11 +1,7 @@
 "use client";
-import dynamic from "next/dynamic";
 import { usePathname } from "next/navigation";
 import { AnimatePresence } from "framer-motion";
-
-const PageMotion = dynamic(() => import("@/components/shared/PageMotion"), {
-  ssr: false,
-});
+import PageMotion from "@/components/shared/PageMotion.tsx";
 
 export default function PageTransition({ children }: { children: React.ReactNode }) {
   const pathname = usePathname();
