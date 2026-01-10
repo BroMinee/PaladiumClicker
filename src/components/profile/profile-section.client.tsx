6849cabe0cea4dbbf90b2dd91ec5022d48bd475c
BroMine <77886876+BroMinee@users.noreply.github.com>
2026-01-10 17:43:44 +0100

    [profil] remove todo from page

--- src/components/profile/profile-section.client.tsx
+++ src/components/profile/profile-section.client.tsx
@@ -17,26 +17,17 @@ export function ProfileSection() {
         <h3 className="text-xl font-semibold mb-2">Description</h3>
         <p className="">{playerInfo.description}</p>
       </div>
-      <div>
+      {/* <div>
         <h3 className="text-xl font-semibold mb-2">Classements</h3>
         <ul className="space-y-2">
           TODO
-          {/* {playerInfo.rankings.map((rank) => (
-                        <li
-                            key={rank.name}
-                            className="flex justify-between p-3 bg-secondary rounded"
-                        >
-                            <span className="">{rank.name}</span>
-                            <span className="font-bold">{rank.rank}</span>
-                        </li>
-                    ))} */}
         </ul>
-      </div>
+      </div> */}
     </div>
-    {/* Colonne 2: Compagnons */}
-    <div className="space-y-4">
+
+    {/* <div className="space-y-4">
       <h3 className="text-xl font-semibold mb-2">Compagnons</h3>
-      {/* <CompanionCard
+      <CompanionCard
                 name={playerInfo.pet?.currentSkin}
                 imageUrl={playerInfo.pet.imageUrl}
                 type="Pet"
@@ -45,8 +36,7 @@ export function ProfileSection() {
                 name={playerData.mount.name}
                 imageUrl={playerData.mount.imageUrl}
                 type="Monture"
-            /> */}
-      TODO
-    </div>
+            />
+    </div> */}
   </div>);
 }
