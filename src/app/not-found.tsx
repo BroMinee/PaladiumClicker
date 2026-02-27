661bbd63b56a0c25a7eae63a3f38c77ebf0c877d
BroMine <77886876+BroMinee@users.noreply.github.com>
2026-02-28 00:43:41 +0100

    [fix] pedro

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -9,7 +9,7 @@ export default function NotFound() {
 
   return (
     <Card
-      className="flex flex-col gap-4 font-bold center bg-no-repeat bg-center bg-cover bg-black justify-center items-center"
+      className="flex flex-col gap-4 font-bold center bg-no-repeat bg-center bg-cover bg-black justify-center items-center text-white"
       style={{ height: "70vh" }}>
       <CardHeader className="flex flex-row gap-2 ">
         <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
@@ -28,7 +28,7 @@ export default function NotFound() {
             <p className="mb-4 text-3xl tracking-tight font-bold md:text-4xl ">Désolé, cette page
               n&apos;existe
               pas.</p>
-            <p className="mb-4 text-lg font-light text-card-foreground">Si vous êtes persuadé que cette page devrait exister,
+            <p className="mb-4 text-lg font-light">Si vous êtes persuadé que cette page devrait exister,
               merci de contacter un développeur.</p>
           </div>
         </div>
