4d03630316271ba75ca8eb41cb4bec70a0d603eb
BroMine <77886876+BroMinee@users.noreply.github.com>
2026-06-17 21:48:42 +0200

    [rework] add the possibility to edit job levels from clicker page

--- src/components/profile/player-jobs.tsx
+++ src/components/profile/player-jobs.tsx
@@ -1,29 +1,5 @@
-import { Card } from "@/components/ui/card";
-import { MetierKey } from "@/types";
-import { JobLevel, JobProgressbar, JobXpCount } from "./player-jobs.client";
-import { prettyJobName } from "@/lib/misc";
+import { JobCard } from "./player-jobs.client";
 import { constants } from "@/lib/constants";
-
-/**
- * Display the job card
- * @param jobName - The job name we are displaying
- */
-export function JobCard({ jobName }: { jobName: MetierKey }) {
-  return (
-    <Card>
-      <div className="flex justify-between items-baseline mb-2">
-        <h3 className="text-lg font-semibold">{prettyJobName(jobName)}</h3>
-        <span className="text-sm font-bold">
-          <JobLevel jobName={jobName}/>
-        </span>
-      </div>
-      <div className="w-full bg-secondary rounded-full h-2.5">
-        <JobProgressbar jobName={jobName}/>
-      </div>
-      <JobXpCount jobName={jobName}/>
-    </Card>
-  );
-}
 
 /**
  * Display all the jobs card
